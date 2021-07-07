b21cc56a30a69061cc497e7281712c68233b6aa4
Kulvin <kkulvinappa@live.com>
2021-07-07 21:34:03 +0400

    change 1

--- test1.py
+++ test1.py
@@ -5,10 +5,21 @@ from bs4 import BeautifulSoup
 page = requests.get(
     'https://www.lalitmauritius.org/en/dictionary.html?letter=a')
 
-soup = BeautifulSoup(page.content, "html.parser")
+soup = BeautifulSoup(page.text, "html.parser")
 
-dict = soup.find('ul', id="dictionarylist")
+# dict = soup.find('span', id="dictionarylist")
 
-headers = [heading.text for heading in dict.find_all('li')]
+main = soup.find_all('span', {"class": "main"})
 
-print(headers)
+desc = soup.find_all('div', {"class": "desc"})
+
+result = main & desc
+
+# main = [kreol.text for kreol in dict.find_all('span')]
+
+# desc = [eng.text for eng in dict.find_all('div')]
+
+# # results = [{main[index]:cell.text for index,
+# #             cell in enumerate(eng.find_all("div"))} for eng in desc]
+
+print(result)
